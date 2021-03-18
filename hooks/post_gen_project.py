55cc3e662933674482ad5eaba934143df26591fd
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-18 19:25:42 +0100

    Added datapack and resourcepack option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,16 +1,22 @@
 import os
 from pathlib import Path
+import shutil
 
 def is_file_empty(file: Path):
     return os.stat(file).st_size == 0
 
-def remove_empty_files():
-
-    root = Path(os.getcwd())
+def remove_empty_files(root: Path):
     for dirpath, _, filenames in os.walk(root):
         for file in [Path(dirpath, f) for f in filenames]:
             if is_file_empty(file):
                 os.remove(file)
 
 if __name__ == "__main__":
-    remove_empty_files()
+    root = Path(os.getcwd())
+
+    remove_empty_files(root)
+
+    if not "{{cookiecutter.datapack}}" == "y":
+        shutil.rmtree(Path(root, "datapack"))
+    if not "{{cookiecutter.resourcepack}}" == "y":
+        shutil.rmtree(Path(root, "resourcepack"))
